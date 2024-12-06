92166058bdda16e79cd34c8be191f36cc898e631
Tomer Zipori <tomerzip@post.bgu.ac.il>
2024-12-06 16:41:50 +0200

    Update stock_scrape.sh

--- stock_scrape.sh
+++ stock_scrape.sh
@@ -9,6 +9,15 @@ PYTHON_INTERPRETER="C:\Users\Tomer\Documents\GitHub\stock_scrape\.venv\Scripts\p
 # Log file location
 LOG_FILE="C:\Users\Tomer\Documents\GitHub\stock_scrape\scrape_errors.log"
 
+# Path to the Git repo
+REPO_DIR="C:\Users\Tomer\Documents\GitHub\stock_scrape"
+
+# File to commit
+CSV_FILE="prices.csv"
+
+# Git branch
+BRANCH="master"
+
 # Print a start message
 echo "Started scraping..."
 
@@ -18,6 +27,21 @@ $PYTHON_INTERPRETER $PYTHON_SCRIPT
 # Check if ran successfully
 if [ $? -eq 0 ]; then
 	echo "Scraping completed successfully"
+
+	#  Navigate to the repository
+	cd "$REP_DIR" || exit
+
+	# Add the csv file to the staging area
+	git add "$CSV_FILE"
+
+	# Commit changes
+	git commit -m "Updated prices.csv"
+
+	# Push changes to Github
+	git push origin "$BRANCH"
+
+	echo "CSV file committed and pushed to Github successfully"
 else
 	echo "Scraping script encountered an error. Check the log file ($LOG_FILE) for details."
 fi
+
