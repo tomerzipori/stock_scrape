52f9e8e8b8455aaed2e37e7e20b8227311a1cd55
Tomer Zipori <tomerzip@post.bgu.ac.il>
2024-12-20 15:21:24 +0200

    Update stock_scrape.sh

--- stock_scrape.sh
+++ stock_scrape.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Navigate to the cloned GitHub repository
+cd /app || exit
+
+# Git configuration
+git config --global user.name "tomerzipori"
+git config --global user.email "tomerzip@post.bgu.ac.il"
+
 # Set the path to the Python script
 PYTHON_SCRIPT="/app/scrape.py"
 
@@ -9,21 +16,17 @@ PYTHON_INTERPRETER="python"
 # Log file location
 LOG_FILE="/app/process_log.txt"
 
-# Path to the Git repo
-REPO_DIR="/app"
-
 # File to commit
 CSV_FILE="/app/prices.csv"
 
 # Git branch
 BRANCH="master"
 
-# Start the log file with a timestamp (append to the file)
+# Start the log file with a timestamp
 echo "Process started at $(date)" >> "$LOG_FILE"
 
-# Run Python script and log output (append to the log file)
+# Run Python script and log output
 echo "Running Python script..." >> "$LOG_FILE"
-
 $PYTHON_INTERPRETER $PYTHON_SCRIPT >> "$LOG_FILE" 2>&1
 
 # Check if the script ran successfully
@@ -31,22 +34,13 @@ if [ $? -eq 0 ]; then
     echo "Scraping completed successfully" >> "$LOG_FILE"
 else
     echo "Scraping script encountered an error. Check the details below:" >> "$LOG_FILE"
+    exit 1
 fi
 
-# End the log file with a timestamp (append to the file)
+# End the log file with a timestamp
 echo "Process ended at $(date)" >> "$LOG_FILE"
 
-# Navigate to the repository
-cd "$REPO_DIR" || exit
-
-# Add the CSV file to the staging area
-git add "$CSV_FILE" >> "$LOG_FILE" 2>&1
-
-# Add the log file to the staging area
-git add "$LOG_FILE" >> "$LOG_FILE" 2>&1
-
-# Commit changes
-git commit -m "Updated prices.csv and process log" >> "$LOG_FILE" 2>&1
-
-# Push changes to GitHub
-git push origin "$BRANCH" >> "$LOG_FILE" 2>&1
+# Commit and push changes to GitHub
+git add "$CSV_FILE" "$LOG_FILE"
+git commit -m "Updated prices.csv and process log"
+git push origin "$BRANCH"
